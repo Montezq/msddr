52d2d523f686cb817890eb83cb7c8eecdfe8c2a6
gavani <intalmaci21@gmail.com>
2020-02-06 02:36:21 +0200

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	$('.header__navbar-menu-icon').click(function(){
+		$(this).toggleClass('open');
+	});
+});
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQSIsImZpbGUiOiJhcHAuanMiLCJzb3VyY2VzQ29udGVudCI6WyIkKGRvY3VtZW50KS5yZWFkeShmdW5jdGlvbigpe1xyXG5cdCQoJy5oZWFkZXJfX25hdmJhci1tZW51LWljb24nKS5jbGljayhmdW5jdGlvbigpe1xyXG5cdFx0JCh0aGlzKS50b2dnbGVDbGFzcygnb3BlbicpO1xyXG5cdH0pO1xyXG59KTsiXX0=
